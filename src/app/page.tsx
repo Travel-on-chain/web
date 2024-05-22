085409bc3e92d96e43b70e68f8e19366bc7658bf
kabudafax <lewisnie123@163.com>
2024-05-23 04:44:45 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import CardList from '@/components/carousel/CardList';
 import { hotDropsData } from '@/constants/MockupData';
 import NightEarth from '@/components/night-earth/night-earth';
 
+export default function Home() {
 	return (
 		<main className="pointer-events-none z-[1] h-full overflow-auto bg-[#111827] bg-transparent">
 			<div className="pointer-events-none relative !z-[3] mx-auto h-full max-w-screen-xl bg-transparent">
